4939a5abbde569781e34c56909f35d739a77ffa2
root <kraval@rarus.ru>
2019-07-30 16:28:37 +0300

    CHECK

--- components/exam2/simplecomp.exam2/class.php
+++ components/exam2/simplecomp.exam2/class.php
@@ -18,21 +18,18 @@ class SimpleComp2 extends CBitrixComponent
     }
     
     // получение и переопределение параметров
-    private function receiveParams()
+    private function handlerParams()
     {
-        $this->arParams['PROD_IBLOCK_ID'] = intval($this->arParams['PROD_IBLOCK_ID']);
-        $this->arParams['CLASS_IBLOCK_ID'] = intval($this->arParams['CLASS_IBLOCK_ID']);
+        $this->arParams['PROD_IBLOCK_ID'] = (int) $this->arParams['PROD_IBLOCK_ID'];
+        $this->arParams['CLASS_IBLOCK_ID'] = (int) $this->arParams['CLASS_IBLOCK_ID'];
         
         if(!$this->arParams['CACHE_TIME']) {
             $this->arParams['CACHE_TIME'] = 3600000;
         }
-<<<<<<< HEAD
         // проверка на наличие параметра 'F' 
         if(isset($_GET["F"])){
             $this->arParams['CACHE_TIME'] = 0;
         }
-=======
->>>>>>> cf6353168793e7362b9533288429f6e2dd715802
     }
     
     // запросы
@@ -50,36 +47,37 @@ class SimpleComp2 extends CBitrixComponent
             'IBLOCK_ID',
             'NAME'
         );
-        $res = CIBlockElement::GetList(array(), $arFilter, $arSelect);
+        $arNavParams = Array(
+            "nPageSize" => $this->arParams["NAV_COUNT"],
+            "bShowAll" => true,
+            "nTopCount"=>5,
+        );
+        $res = CIBlockElement::GetList(array('id'=>'asc'), $arFilter, $arNavParams, $arSelect);
         while ($ob = $res->Fetch())
         {
             $this->firms[] = $ob;
+            //echo'<pre>';var_dump($ob);echo'</pre>';
         }
+        
+        $this->arResult["NAV_STRING"] = $res->GetPageNavStringEx($navComponentObject, "", "", true); //постраничная
         
         // получение елементов каталога и изменение url детального просмотра
         $arSort = array('NAME' => 'ASC', 'SORT' => 'ASC');
-<<<<<<< HEAD
         // добавление фильтра F
         $filter = array();
         if(isset($_GET["F"])){
-            $filtr = array(
+            $filter = array(
                 "LOGIC" => "OR",
                 array("<=PROPERTY_PRICE" => 1700, "PROPERTY_MATERIAL" => "Дерево, ткань"),
                 array("<PROPERTY_PRICE" => 1500, "PROPERTY_MATERIAL" => "Металл, пластик"),
             );
         }
-=======
->>>>>>> cf6353168793e7362b9533288429f6e2dd715802
         $arFilter = array(
             'IBLOCK_ID' => $this->arParams['PROD_IBLOCK_ID'],
             'ACTIVE' => 'Y',
             'CHECK_PERMISSIONS' => 'Y',
-<<<<<<< HEAD
             '!PROPERTY_'.$this->arParams['ELEMENT_PROP_CODE'] => false,
             $filter
-=======
-            '!PROPERTY_'.$this->arParams['ELEMENT_PROP_CODE'] => false
->>>>>>> cf6353168793e7362b9533288429f6e2dd715802
         );
         $arSelect = array(
             'ID',
@@ -93,33 +91,73 @@ class SimpleComp2 extends CBitrixComponent
         );
         $res = CIBlockElement::GetList($arSort, $arFilter, false, false, $arSelect);
         $res->SetUrlTemplates($this->arParams['DETAIL_URL_TEMPLATE']);
+        $arrPrice = array();
         while ($ob = $res->GetNext())
         {
-            $this->elems[] = $ob;
+            $arItem = $ob;
+            $arrPrice[] = $ob['PROPERTY_PRICE_VALUE'];
             
+            $arButtons = CIBlock::GetPanelButtons(
+                $arItem['IBLOCK_ID'],
+                $arItem["ID"],
+                0,
+                array("SECTION_BUTTONS"=>false, "SESSID"=>false)
+            );
+            
+            $arItem["EDIT_LINK"] = $arButtons["edit"]["edit_element"]["ACTION_URL"];
+            $arItem["DELETE_LINK"] = $arButtons["edit"]["delete_element"]["ACTION_URL"];
+            $arItem["ADD_LINK"] = $arButtons["edit"]["add_element"]["ACTION_URL"];
+            
+            $this->elems[] = $arItem;
+            
+            //echo '<pre>';var_dump($arButtons);echo '</pre>';
         }
+        $this->arResult['MIN_PRICE'] = min($arrPrice);
+        $this->arResult['MAX_PRICE'] = max($arrPrice);
+        //echo '<pre>';var_dump($arrPrice);echo '</pre>';
     }
     
     // формирование arResult
     private function setResult()
     {
         // проходим циклом по фирмам в arResult
-        foreach($this->firms as $key => $firm)
+        foreach($this->firms as $fKey => $firm)
         {
-            $this->arResult['FIRMS'][$key]['NAME'] = $firm['NAME']; 
+            $this->arResult['FIRMS'][$fKey]['NAME'] = $firm['NAME'];
+            $this->arResult['FIRMS'][$fKey]['IBLOCK_ID'] = $firm['NAME'];
             // проходим циклом по елементам
-            foreach($this->elems as $elem)
+            foreach($this->elems as $eKey => $elem)
             {
                 // если элемент относится к этой фирме, добавляем елемент в фирму
                 if(intval($firm['ID']) === intval($elem['PROPERTY_'.$this->arParams['ELEMENT_PROP_CODE'].'_VALUE']))
                 {
                     $arElemProps = implode(' - ', array($elem['NAME'], $elem['PROPERTY_PRICE_VALUE'], $elem['PROPERTY_MATERIAL_VALUE'], $elem['PROPERTY_ARTNUMBER_VALUE']));
-                    $this->arResult['FIRMS'][$key]['PRODUCTS'][] = $arElemProps . '('.$elem['DETAIL_PAGE_URL'].')';
+                    $this->arResult['FIRMS'][$fKey]['PRODUCTS'][$eKey]['PROPS'] = $arElemProps . '('.$elem['DETAIL_PAGE_URL'].')';
+                    $this->arResult['FIRMS'][$fKey]['PRODUCTS'][$eKey]['ADD_LINK'] = $elem['ADD_LINK'];
+                    $this->arResult['FIRMS'][$fKey]['PRODUCTS'][$eKey]['EDIT_LINK'] = $elem['EDIT_LINK'];
+                    $this->arResult['FIRMS'][$fKey]['PRODUCTS'][$eKey]['DELETE_LINK'] = $elem['DELETE_LINK'];
+                    $this->arResult['FIRMS'][$fKey]['PRODUCTS'][$eKey]['IBLOCK_ID'] = $elem['IBLOCK_ID'];
+                    $this->arResult['FIRMS'][$fKey]['PRODUCTS'][$eKey]['ID'] = $elem['ID'];
                 }
             }
         }
         
         //echo '<pre>';var_dump($this->arResult);echo '</pre>';
+    }
+    
+    // ДОБАВЛЕНИЕ КНОПКИ В ПОПАП КОМПОНЕНТА
+    private function bAdd()
+    {
+        $res = CIBlock::GetByID($this->arParams['PROD_IBLOCK_ID']);
+        $this->AddIncludeAreaIcons(
+            Array( //массив кнопок toolbar'a
+                Array(
+                    "ID" => "SIMPLECOMP_BUTTON",
+                    "TITLE" => 'ИБ в Админке',
+                    "URL" => '/bitrix/admin/iblock_element_admin.php?IBLOCK_ID=' . $this->arParams['PROD_IBLOCK_ID'] .'&type='.$res->Fetch()['IBLOCK_TYPE_ID'].'&lang='. LANGUAGE_ID .'&find_el_y=Y',
+                )
+            )
+        );	
     }
     
     // исполнение компонента
@@ -130,11 +168,14 @@ class SimpleComp2 extends CBitrixComponent
          
         if ($this -> StartResultCache(false, $USER->GetGroups()))
         {
-            $this -> receiveParams();
+            $this -> handlerParams();
             $this -> firmsList();
             $this -> setResult();
             $this -> SetResultCacheKeys(array(
                 'FIRMS',
+                'MIN_PRICE',
+                'MAX_PRICE',
+                'NAV_STRING',
             ));
             $this -> IncludeComponentTemplate();
         }
@@ -143,7 +184,13 @@ class SimpleComp2 extends CBitrixComponent
 		$this->AbortResultCache();
 	}
         global $APPLICATION;
+        if ($APPLICATION->GetShowIncludeAreas())
+		{
+            $this -> bAdd();
+        }
         $APPLICATION -> SetTitle(GetMessage('PAGE_TITLE'). '['.count($this->arResult['FIRMS']).']');
+        $APPLICATION -> AddViewContent('min_price', $this->arResult['MIN_PRICE']);
+        $APPLICATION -> AddViewContent('max_price', $this->arResult['MAX_PRICE']);
     }
 }
 ?>
